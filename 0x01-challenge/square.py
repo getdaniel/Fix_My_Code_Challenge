1a270a401f9be4cd3e9edeea49f05d0274fbbc02
ShagizMan <danielgetaneh2011@gmail.com>
2022-10-06 20:35:11 +0300

    fix the style of defining function name

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -16,7 +16,7 @@ class Square():
         """ Area of the square """
         return self.width * self.height
 
-    def PermiterOfMySquare(self):
+    def permiter_of_my_square(self):
         """ Perimeter of the square."""
         return (self.width * 2) + (self.height * 2)
 
@@ -30,4 +30,4 @@ if __name__ == "__main__":
     s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.permiter_of_my_square())
