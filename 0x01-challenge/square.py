fb4c0a243fb1ac8b4bd32a5c8224e4c6aefe36da
ShagizMan <danielgetaneh2011@gmail.com>
2022-10-06 20:30:20 +0300

    my square - pass the pycodestyle standarf

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -3,11 +3,12 @@
 
 
 class Square():
-    """Square class."""
+    """ Square class."""
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
+        """ Instantiation of class."""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -16,14 +17,16 @@ class Square():
         return self.width * self.width
 
     def PermiterOfMySquare(self):
+        """ Perimeter of the square."""
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """ Printable representation of square object."""
         return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
-
+    """ Creates Square object."""
     s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
